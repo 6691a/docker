0f4538a453032ea71a29db287aa9d1f25b938548
6691a <6691a@naver.com>
2022-02-07 15:55:03 +0900

    jenkins test setting

---
+++ jenkins/make.py
@@ -0,0 +1,10 @@
+"""
+v0.0.1
+2022.02.07
+"""
+import subprocess, os
+
+DOCKER_GROUP_ID = subprocess.check_output("getent group docker", shell=True, text=True)
+
+SERVICE = "jenkins"
+os.putenv('DOCKER_GROUP_ID', DOCKER_GROUP_ID)
