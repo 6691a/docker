1f8ac225bc5aadbc9a800ac9ad3b2c6dca7a0cf5
6691a <6691a@naver.com>
2022-02-07 16:12:37 +0900

    jenkins setting 수정

--- jenkins/make.py
+++ jenkins/make.py
@@ -7,4 +7,7 @@ import subprocess, os
 DOCKER_GROUP_ID = subprocess.check_output("getent group docker", shell=True, text=True)
 
 SERVICE = "jenkins"
+
 os.putenv('DOCKER_GROUP_ID', DOCKER_GROUP_ID)
+
+subprocess.run("docker-compose up --build -d", shell=True)
